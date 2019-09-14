b53e7ea5c7ab9c86fe251b5ed36d58b41117df81
Velox <veloxization@gmail.com>
2019-09-14 21:29:53 +0300

    Added comments

--- bot.py
+++ bot.py
@@ -12,7 +12,7 @@ from PIL import Image, ImageDraw, ImageFont
 from pathlib import Path
 from discord.ext import commands
 
-TOKEN = 'NjEyNzE4MjU1NjM4ODM5MzA4.XVmccQ.nBZB73prpOvrIab84VMjGj0BFes'
+TOKEN = 'NjEyNzE4MjU1NjM4ODM5MzA4.XVmccQ.nBZB73prpOvrIab84VMjGj0BFes' # Not a real token. Demonstration purposes only.
 bot = commands.Bot(command_prefix='$')
 
 @bot.event
@@ -62,6 +62,7 @@ async def on_member_join(member):
 @bot.command()
 @commands.has_permissions(kick_members=True)
 async def kick(ctx, members: commands.Greedy[discord.Member], *reason: str):
+    # Command still unfinished
     kicked_list = ", ".join(member.name for member in members)
     full_reason = " ".join(word for word in reason)
     await ctx.send("{} not kicked because the command is not ready yet but the reason was {}".format(kicked_list, full_reason))
@@ -69,19 +70,25 @@ async def kick(ctx, members: commands.Greedy[discord.Member], *reason: str):
 @bot.command()
 @commands.has_permissions(ban_members=True)
 async def ban(ctx, member: typing.Union[discord.User, int], days: typing.Optional[int] = 0, *reason: str):
+    # Initialise the member ID just in case
     memberid = 0
+    # Check if the user entered an ID or tagged a member
     if type(member) is int:
         memberid = member
     else:
         memberid = member.id
+    # The bot can't remove more messages than past 7 days, so change any number greater than 7 to 7
     if days > 7:
         days = 7
+    # Combine the reason so the user doesn't have to use quotations
     full_reason = " ".join(word for word in reason)
+    # Ban the member, remove the messages and delete the specified amount of messages (default 0), also inform about the action
     await ctx.message.guild.ban(discord.Object(id=memberid), reason=full_reason, delete_message_days=days)
     await ctx.send("<@{}> was banned.".format(memberid))
     print("{.author} banned {} with reason: {}".format(ctx, member, full_reason))
 @ban.error
 async def ban_error(ctx, error):
+    # Catch any errors if the user enters an invalid name or ID
     if isinstance(error, discord.ext.commands.BadUnionArgument):
         await ctx.send("Member not found.")
     elif isinstance(error, discord.ext.commands.errors.CommandInvokeError):
@@ -92,6 +99,7 @@ async def ban_error(ctx, error):
 @bot.command()
 @commands.has_permissions(ban_members=True)
 async def unban(ctx, memberid: int, *reason):
+    # Combine the reason so the user doesn't have to use quotations, unban the member and inform about it
     full_reason = " ".join(word for word in reason)
     await ctx.guild.unban(discord.Object(id=memberid), reason=full_reason)
     await ctx.send("<@{}> was unbanned.".format(memberid))
@@ -100,22 +108,26 @@ async def unban(ctx, memberid: int, *reason):
 @bot.command()
 @commands.has_permissions(ban_members=True)
 async def warn(ctx, member: discord.Member, *reason: str):
+    # Check if the user tagged a proper member
     try:
         warnedid = member.id
     except:
         ctx.send("Member not found.")
-    warnedmention = member.mention
+    # Combine the reason so the user doesn't have to use quotations
     full_reason = " ".join(word for word in reason)
+    # Check that the user actually provides a reason
     if full_reason == "":
         await ctx.send("Please include a reason for logging purposes.")
         return
+    # Connect to the Sqlite database and save the warning there
     conn = sqlite3.connect('fhbot.db')
     cursor = conn.cursor()
     cursor.execute('INSERT INTO warnings (memberid, reason, issuerid) VALUES (?, ?, ?)', (warnedid, full_reason, ctx.message.author.id))
     conn.commit()
     conn.close()
+    # Inform about the warning and send a message to the user who was warned
     print("{.author} warned {} for: {}".format(ctx, member, full_reason))
-    await ctx.send("{} was warned.".format(warnedmention))
+    await ctx.send("{.mention} was warned.".format(member))
     await member.send("You were warned for: {}".format(full_reason))
 
 @bot.command()
@@ -168,14 +180,17 @@ async def removewarn(ctx, arg):
 @bot.command()
 @commands.has_permissions(ban_members=True)
 async def warnings(ctx, member: discord.Member):
+    # Check if the user tagged a proper member
     try:
         memberid = member.id
     except:
         await ctx.send("Member not found.")
         return
+    # Connect to the database and get the warnings of the specified user
     conn = sqlite3.connect('fhbot.db')
     cursor = conn.cursor()
     title = "Warnings of {.display_name}".format(member)
+    # Collect the warnings into an embed and show the warning's ID, issuer and the reason
     description = ""
     for row in cursor.execute('SELECT warningid, reason, issuerid FROM warnings WHERE memberid=?', (memberid,)):
         description += "\n"
@@ -187,22 +202,32 @@ async def warnings(ctx, member: discord.Member):
 @bot.command()
 @commands.has_permissions(manage_roles=True)
 async def mute(ctx, member: discord.Member, *time: typing.Optional[str]):
+    # Used for checking if the muted role has already been created
     role_exists = False
+    # Create the permissions object for the role
     perms = discord.Permissions(permissions=0)
+    # Initialise the role, just in case
     muted_role = None
+    # Check the roles to see if the FHmuted role is already created
     for role in ctx.guild.roles:
         if role.name == "FHmuted":
             role_exists = True
             muted_role = role
+    # If the role is not already created, create it with permissions created above
     if not role_exists:
         muted_role = await ctx.guild.create_role(name="FHmuted", permissions=perms)
+    # Create channel overrides for the muted role so the person can't send messages, files or add reactions. They also can't speak, connect or stream on voice channels
     for channel in ctx.guild.text_channels:
         await channel.set_permissions(muted_role, send_messages=False, add_reactions=False, send_tts_messages=False, attach_files=False)
     for channel in ctx.guild.voice_channels:
         await channel.set_permissions(muted_role, speak=False, connect=False, stream=False)
+    # Add the created role to the muted member
     await member.add_roles(muted_role)
+    # If time was specified, go for the timed unmute
     if time is not None:
+        # The smallest specifiable unit is minutes
         minutes = 0
+        # Convert each unit to minutes and check that the time does not exceed 7 days
         for timeunit in time:
             if "d" in timeunit:
                 days = int(timeunit.split("d")[0])
@@ -229,22 +254,29 @@ async def mute(ctx, member: discord.Member, *time: typing.Optional[str]):
             if minutes > 10080:
                 await ctx.send("Time cannot exceed 7 days.")
                 return
+        # Get current time in UTC to calculate the end time of the mute
         start_date = datetime.datetime.utcnow()
         end_date = start_date + datetime.timedelta(minutes=minutes)
+        # Inform that the member will be muted until end_date
         await ctx.send("{.name} will be muted until {}".format(member, end_date.strftime("%B %d, %I:%M %p UTC")))
+        # Wait for the unmute action
         await asyncio.sleep(minutes * 60)
         await member.remove_roles(muted_role)
         print("{.name} was unmuted.".format(member))
     else:
+        # If no time was specified, the member will be muted indefinitely
         await ctx.send("{.name} was muted.".format(member))
 
 @bot.command()
 @commands.has_permissions(kick_members=True)
 async def testwelcome(ctx):
+    # For testing the welcoming image
     await on_member_join(ctx.author)
 
 @bot.command()
+# Command for listing all available commands and their function
 async def commands(ctx, *arg):
+    # List all admin commands
     if "".join(arg).lower() == "admin":
         embed = discord.Embed(title="Admin commands", description="[optional]", color=0x7f00ff)
         embed.add_field(name="kick @member [reason]", value="Kicks a member with an optional reason.", inline=False)
@@ -257,6 +289,7 @@ async def commands(ctx, *arg):
         embed.add_field(name="testwelcome", value="Displays the welcoming message as if a new member joined.", inline=False)
         await ctx.send(embed=embed)
     else:
+        # If no category is specified, list all available categories
         await ctx.send("Usage: commands [category]\n__**Categories**__\n**Admin**: Admin commands")
 
 
